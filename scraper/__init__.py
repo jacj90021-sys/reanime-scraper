49aa9b89a18d11ca3a6c2a56c70ec6b72af2fdfc
jacj90021-sys <jacj90021@gmail.com>
2026-08-04 00:13:22 +0600

    add scraper/__init__.py

---
+++ scraper/__init__.py
@@ -0,0 +1,9 @@
+"""Scraper for reanime.to / reanime.cz (Re:ANIME mirror site)."""
+
+from .reanime import (
+    scrape_anime,
+    scrape_homepage,
+    scrape_video_urls,
+    run,
+    session,
+)
